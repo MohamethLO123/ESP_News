4bb92b09fb19078b893923f8d4c9490fef0ec03e
MohamethLO123 <mohamethlo01@esp.sn>
2025-04-09 09:27:54 +0000

    Mis en place du header et du footer

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualités ESP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+
+    <nav class="css-navbar navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+    <a class="navbar-brand d-flex align-items-center mx-4 py-3" href="index.php">
+    <div style="background-color: white; padding: 4px; border-radius: 6px;">
+        <img src="img/logo_ESP.png" alt="Logo ESP" style="height: 40px; width: auto;">
+    </div>
+    <span class="ms-2 text-white">Actualités ESP</span>
+</a>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#">Catégories</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Articles</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container mt-4">
+</div> 
--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil ESP</title>
-</head>
-<body>
-    <h1>Site pour les actualite de l'ESP</h1>
-</body>
-</html>
+<?php
+    include "header.php";
+?>
+
+
+
+<?php
+    include "footer.php";
+?>
